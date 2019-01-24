e958e003588ae9b3ae10c3cc1a4840d2ef0a5e12
Benjamin Fransson <benjamin@cafcon.se>
2019-01-24 20:39:45 +0100

    Refactored config

--- rollup.config.js
+++ rollup.config.js
@@ -1,26 +1,27 @@
 import browsersync from 'rollup-plugin-browsersync';
 import postcss from 'rollup-plugin-postcss';
 import postcssNormalize from 'postcss-normalize';
+import autoprefixer from 'autoprefixer';
 import cssnano from 'cssnano';
 
-const isProduction = process.env.NODE_ENV === 'production';
+const isProduction  = process.env.NODE_ENV === 'production';
 const isDevelopment = isProduction === false;
 
 
 module.exports = {
-  input: 'src/scripts/index.js',
+  input : 'src/scripts/index.js',
   output: {
-    file: 'public/giphy.js',
+    file  : 'public/giphy.js',
     format: 'iife'
   },
   plugins: [
     postcss({
-      extract: true,
+      extract  : true,
       sourceMap: true,
-      plugins: [
+      plugins  : [
         postcssNormalize(),
-        require('autoprefixer'),
-        require('cssnano')({
+        autoprefixer(),
+        cssnano({
           preset: 'default',
         })
       ]
