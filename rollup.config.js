420e123e6a0a6cc1978740ac91b9e674a57683cf
Benjamin Fransson <benjamin@cafcon.se>
2019-01-24 23:16:05 +0100

    Resolved merge conflict

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,10 @@
 import browsersync from 'rollup-plugin-browsersync';
 import postcss from 'rollup-plugin-postcss';
 import postcssNormalize from 'postcss-normalize';
+import commonjs from 'rollup-plugin-commonjs';
+import resolve from 'rollup-plugin-node-resolve';
+import filesize from 'rollup-plugin-filesize';
+import { terser } from "rollup-plugin-terser";
 import autoprefixer from 'autoprefixer';
 import cssnano from 'cssnano';
 import injectEnv from 'rollup-plugin-inject-env';
@@ -17,6 +21,9 @@ module.exports = {
     sourcemap: true
   },
   plugins: [
+    (isProduction && terser()),
+    (isProduction && filesize()),
+    (isDevelopment && browsersync({server: 'public'})),
     postcss({
       extract  : true,
       sourceMap: true,
@@ -32,5 +39,15 @@ module.exports = {
     injectEnv({
       envFilePath: ".env",
     }),
+    resolve({
+      jsnext        : true,
+      browser       : true,
+      preferBuiltins: false,
+    }),
+    commonjs({
+      include     : 'node_modules/**',
+      ignoreGlobal: false,
+      sourceMap   : true,
+    })
   ]
 }
