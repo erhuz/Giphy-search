e9cbe804d8ea7c38ca49bbf3fc64ce6e345c8372
Benjamin Fransson <benjamin@cafcon.se>
2019-01-24 23:13:28 +0100

    Split logic up into separate files

---
+++ src/scripts/giphy.js
@@ -0,0 +1,23 @@
+'use strict';
+
+import queryString from 'query-string';
+
+const buildURL = (query, apiKey, rating) => {
+  const search = queryString.stringify({
+    q      : query,
+    api_key: apiKey,
+    rating : rating
+  })
+
+  return `https://api.giphy.com/v1/gifs/search?${search}`;
+}
+
+const search = (query, apiKey, rating) => {
+  const url = buildURL(query, apiKey, rating);
+
+  return window.fetch(url)
+  .then(res => res.json())
+  .catch(console.error());
+}
+
+export default { search };
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,23 @@
 'use strict';
 
 import '../styles/index.scss';
+import giphy from './giphy';
+
+const apiKey      = process.env.GIPHY_API_KEY;
+const searchForm  = document.querySelector('form#search');
+const searchField = document.querySelector('form .search__field');
+// const searchBtn   = document.querySelector('form .search__button');
+
+searchForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const searchQuery = searchField.value;
+
+  // Search query, Api key, Gif rating
+  giphy.search(searchQuery, apiKey, 'g')
+    .then(res => {
+      console.log(res);
+
+
+    });
+});
