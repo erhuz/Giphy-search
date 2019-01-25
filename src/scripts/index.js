7b24d0398a34ba13ce19e8cb5a7ea70bcb39e7e8
Benjamin Fransson <benjamin@cafcon.se>
2019-01-25 10:42:10 +0100

    Removed btn reference, added gallery reference

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -6,17 +6,37 @@ import giphy from './giphy';
 const apiKey      = process.env.GIPHY_API_KEY;
 const searchForm  = document.querySelector('form#search');
 const searchField = document.querySelector('form .search__field');
-// const searchBtn   = document.querySelector('form .search__button');
+const gallery     = document.querySelector('.gallery');
 
 searchForm.addEventListener('submit', (event) => {
   event.preventDefault();
 
   const searchQuery = searchField.value;
 
+  gallery.innerHTML = '';
+
   // Search query, Api key, Gif rating
   giphy.search(searchQuery, apiKey, 'g')
     .then(res => {
       console.log(res);
+
+
+      res.data.forEach(gif => {
+        gallery.innerHTML += gif.images.original.url;
+        log(gif);
+        const linkTag = document.createElement('a');
+        log(linkTag);
+        linkTag.classList.add('gallery__item');
+
+        const img = document.createElement('img');
+        img.setAttribute('src', gif.images.original.url);
+
+        linkTag.appendChild(img);
+
+        log('this workin?');
+
+
+      });
 
 
     });
